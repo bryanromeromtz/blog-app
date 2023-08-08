50f082e655796a0cc6d650a0a12767438dd3a190
Madzilla <cryptomtz@hotmail.com>
2023-08-08 12:49:56 -0600

    Adding new services and deployments to kubernetes cluster, to communicate our microservices with each other

--- query/index.js
+++ query/index.js
@@ -54,7 +54,7 @@ app.listen(4002, async () => {
   console.log("Listening on 4002");
 
   // Get all events from event-bus
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
 
   // Replay all events
   for (let event of res.data) {
